ac2a95d1b8f941b50cabd7cc2e9484993c5f1a42
Morten Vassvik <mvassvik@gmail.com>
2017-11-13 21:40:54 +0100

    Pass on the vertex position to th efragment shader (it gets interpolated)

--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -2,7 +2,10 @@
 
 uniform float time;
 
+out vec3 fragment_position;
+
 void main() {
+	// base vertex position
 	vec3 p;
 	if (gl_VertexID == 0)
 		p = vec3(-0.5, -0.5, 0.0);
@@ -11,8 +14,14 @@ void main() {
 	else
 		p = vec3(0.0, 0.5, 0.0);
 
-	float freq = 2.0*3.1416;
+	// transform vertex position
+	float freq = 3.1416;
+	p.xy *= 1.0 + 0.3*cos(2.0*freq*time);
 	p.xy += vec2(0.2*cos(freq*time), 0.2*sin(freq*time));
 
+	// output transformed vertex position
 	gl_Position = vec4(p, 1.0);
+
+	// pass through position to fragment shader
+	fragment_position = p;
 }
