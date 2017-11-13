5816f0233b2dd777de1446c0eb665742b423c7ab
Morten Vassvik <mvassvik@gmail.com>
2017-11-13 22:33:24 +0100

    Added color inversion inside a maximum fitting circle

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,9 +1,13 @@
 #version 330 core
 
-in vec3 in_color;
+in vec3 fragment_position;
+in vec3 fragment_color;
 
 out vec4 color;
 
 void main() {
-	color = vec4(in_color, 1.0);
+	if (length(fragment_position - vec3(0.0, -0.19, 0.0)) < 0.31)
+		color = vec4(1.0 - fragment_color, 1.0);
+	else
+		color = vec4(fragment_color, 1.0);
 }
--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -2,7 +2,8 @@
 
 uniform float time;
 
-out vec3 in_color;
+out vec3 fragment_position;
+out vec3 fragment_color;
 
 void main() {
 	// base vertex position
@@ -13,6 +14,9 @@ void main() {
 		p = vec3(0.5, -0.5, 0.0);
 	else
 		p = vec3(0.0, 0.5, 0.0);
+	
+	// pass through position to fragment shader
+	fragment_position = p;
 
 	// transform vertex position
 	float freq = 3.1416;
@@ -22,12 +26,13 @@ void main() {
 	// output transformed vertex position
 	gl_Position = vec4(p, 1.0);
 
-	// pass through position to fragment shader
+	// pass through color to fragment shader
 	if (gl_VertexID == 0)
-		in_color = vec3(1.0, 0.0, 0.0);
+		fragment_color = vec3(1.0, 0.0, 0.0);
 	else if (gl_VertexID == 1)
-		in_color = vec3(0.0, 1.0, 0.0);
+		fragment_color = vec3(0.0, 1.0, 0.0);
 	else
-		in_color = vec3(0.0, 0.0, 1.0);
+		fragment_color = vec3(0.0, 0.0, 1.0);
+
 		
 }
