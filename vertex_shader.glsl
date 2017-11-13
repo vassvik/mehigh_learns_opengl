b07b9a78d44716a62c2b2e326dd1fcee19abac85
Morten Vassvik <mvassvik@gmail.com>
2017-11-13 20:43:22 +0100

    Added hard coded vertex data to the shader

--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -1,5 +1,13 @@
 #version 330 core
 
 void main() {
-	
+	vec3 p;
+	if (gl_VertexID == 0)
+		p = vec3(-0.5, -0.5, 0.0);
+	else if (gl_VertexID == 1)
+		p = vec3(0.5, -0.5, 0.0);
+	else
+		p = vec3(0.0, 0.5, 0.0);
+
+	gl_Position = vec4(p, 1.0);
 }
